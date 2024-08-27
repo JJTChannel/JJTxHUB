f6f0921f038c50407741cbcc08b9fa1d78290d0e
Buffalo Store <95204359+JJTChannel@users.noreply.github.com>
2024-08-27 21:51:06 +0700

    Update Re-come.lua

--- Re-come.lua
+++ Re-come.lua
@@ -1,4 +1,29 @@
-loadstring(game:HttpGet("https://pi-hub3-14.vercel.app/sc/hub/a.lua"))()
+local req = (syn and syn.request) or (http and http.request) or http_request
+if req then
+    local success, response = pcall(function()
+        return req({
+            Url = 'http://127.0.0.1:6463/rpc?v=1',
+            Method = 'POST',
+            Headers = {
+                ['Content-Type'] = 'application/json',
+                Origin = 'https://discord.com'
+            },
+            Body = game:GetService('HttpService'):JSONEncode({
+                cmd = 'INVITE_BROWSER',
+                nonce = game:GetService('HttpService'):GenerateGUID(false),
+                args = {code = 'dVSFmJYDdS'}
+            })
+        })
+    end)
+
+    if not success then
+        warn('Request failed: ' .. response)
+    else
+        print('Request succeeded: ', response)
+    end
+else
+    warn('Request method not found.')
+end
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))() 
 local Player = game.Players.LocalPlayer
